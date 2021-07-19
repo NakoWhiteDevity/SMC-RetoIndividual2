24734946feee5c0048529707ef28a6970c4ceb8e
CarlosFraile <carlosfrailegomez93@gmail.com>
2021-07-19 12:05:23 +0200

    Primer achuche a esta hackaton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { BarralateralComponent } from './componentes/barralateral/barralateral.component';
+import { PrincipalComponent } from './componentes/principal/principal.component';
+
+const routes: Routes = [
+  {path:'',component:PrincipalComponent},
+  {path:'**',redirectTo:'',pathMatch:'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarralateralComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
